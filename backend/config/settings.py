752cff34e2fc4670a851faac8be94a0a5573f852
MrVarrow <franekvarrow@gmail.com>
2026-04-26 23:41:04 +0200

    Fixed circleci pipeline

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "0") == "1"
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["*"]
 
